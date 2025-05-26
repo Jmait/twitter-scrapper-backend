cf0c52255504cbd8b3a0db184bbd4dcd4390efda
Mr Ahmad <cheapestorderconcept@gmail.com>
2025-05-26 19:06:24 +0100

    fix memory issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,8 +14,17 @@ import { Tweets, UserSchema } from './db/tweets.schema';
     ConfigModule.forRoot({
       isGlobal:true
     }),
-    MongooseModule.forRoot('mongodb+srv://tweetsget:SUz2OTbFrI0zJ7Lw@cluster0.x9hleaq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
-    MongooseModule.forFeature([{ name: Tweets.name, schema: UserSchema }]),
+    MongooseModule.forRoot('mongodb+srv://tweetsget:SUz2OTbFrI0zJ7Lw@cluster0.x9hleaq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
+      maxPoolSize: 10,        // Maximum connections in pool
+        minPoolSize: 2,         // Minimum connections in pool
+        maxIdleTimeMS: 30000,
+       serverSelectionTimeoutMS: 5000,  // How long to try selecting server
+        socketTimeoutMS: 45000,          // How long to wait for socket
+        connectTimeoutMS: 10000,   
+         compressors: ['zlib'],
+        zlibCompressionLevel: 6,
+    }),
+    MongooseModule.forFeature([{ name: Tweets.name, schema: UserSchema, collection: 'tweets' }]),
     ScheduleModule.forRoot({})
   ],
   controllers: [AppController,TwitterController],
